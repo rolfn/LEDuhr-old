c92fe667c114a4ac212506fb8ac5c82d7c96a70b
Rolf Niepraschk <Rolf.Niepraschk@gmx.de>
2014-10-06 21:46:39 +0200

    long keypress toggles sec/date mode

--- LEDuhr.c
+++ LEDuhr.c
@@ -14,9 +14,11 @@
 #include "RN-utils.h"
 
 #define DEBUG
-#define SHOW_SECONDS
 
 wakeup_time wakeupTime;
+uint8_t modeFlag = 0;
+
+#define  SHOW_SECONDS 0
 
 void getWakeupTime(void) {
   uint8_t val = 0, tmp;
@@ -75,10 +77,6 @@ int main(void) {
   SS_Init(LED_DISP_2);                  //   initialize HT16K33 LED controller
   SS_SetBrightness(LED_DISP_1, 4);
   SS_SetBrightness(LED_DISP_2, 0);
-#ifdef SHOW_SECONDS
-  SS_BlankDigit(LED_DISP_2, 0);
-  SS_BlankDigit(LED_DISP_2, 1);
-#endif
   SS_SetColon(LED_DISP_1, 1);
   SS_SetColon(LED_DISP_2, 0);
 
@@ -111,13 +109,15 @@ int main(void) {
     if( timeflags & 1<<ONE_TICK ) { // Call every 1s/64 = 15.625ms
       timeflags &= ~(1<<ONE_TICK);
       keyscan();
+      getKey(1<<THEKEY);
 #ifdef DEBUG
+      /*
       if (tickCnt & 0x80) { // 128 * 15.625ms = 2000ms
         tickCnt = 0;
         lcd_printlc(1, 10, " ");
       }
       tickCnt += 1;
-      getKey(1<<THEKEY);
+      */
       if ( (key==SHORT_KEY) || (key==LONG_KEY) || (key==VERYLONG_KEY) ) {
         switch (key) {
           case SHORT_KEY:
@@ -125,12 +125,15 @@ int main(void) {
             break;
           case LONG_KEY:
             lcd_printlc(1, 10, "L");
+            modeFlag ^= (1<<SHOW_SECONDS); // toggle between date and seconds
             break;
           case VERYLONG_KEY:
             lcd_printlc(1, 10, "V");
             break;
         }
         key = NO_KEY;
+        _delay_ms(2000);
+        lcd_printlc(1, 10, " ");
       }
 #endif
     }
@@ -163,18 +166,20 @@ int main(void) {
       SS_SetDigit(LED_DISP_1, 1, time.hour % 10);
       SS_SetDigit(LED_DISP_1, 2, time.minute / 10);
       SS_SetDigit(LED_DISP_1, 3, time.minute % 10);
-      // second LED display: date or seconds
-#ifdef SHOW_SECONDS
-      SS_SetDigit(LED_DISP_2, 2, time.second / 10);
-      SS_SetDigit(LED_DISP_2, 3, time.second % 10);
-#else
-      SS_SetDigit(LED_DISP_2, 0, time.day / 10);
-      SS_SetDigit(LED_DISP_2, 1, time.day % 10 | SET_DP);
-      SS_SetDigit(LED_DISP_2, 2, time.month / 10);
-      SS_SetDigit(LED_DISP_2, 3, time.month % 10);
-#endif
-
-      if(time.second == 2) {
+      // second LED display: date or secondsS
+      if ( modeFlag & 1<<SHOW_SECONDS ) {
+        SS_BlankDigit(LED_DISP_2, 0);
+        SS_BlankDigit(LED_DISP_2, 1);
+        SS_SetDigit(LED_DISP_2, 2, time.second / 10);
+        SS_SetDigit(LED_DISP_2, 3, time.second % 10);
+      } else {
+        SS_SetDigit(LED_DISP_2, 0, time.day / 10);
+        SS_SetDigit(LED_DISP_2, 1, time.day % 10 | SET_DP);
+        SS_SetDigit(LED_DISP_2, 2, time.month / 10);
+        SS_SetDigit(LED_DISP_2, 3, time.month % 10);
+      }
+#ifdef DEBUG
+      if(time.second == 1) {// some statistics
         if ( dcf77error == 0 ) s_good++;
         else s_bad++;
         quality = ((uint32_t)s_good * (uint32_t)1000) / (uint32_t)(s_good+s_bad);
@@ -182,7 +187,7 @@ int main(void) {
         printDEC_L(2, 16, s_bad);
         printDEC_L(1, 12, (uint16_t)quality);
       }
-
+#endif
       getWakeupTime();
       printDEC(2, 1, wakeupTime.hour);
       lcd_printlc(2, 4, ":");
